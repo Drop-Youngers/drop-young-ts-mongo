b0c9390d4933601b87f416c5dcf81cd3549edca8
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-08 18:59:05 +0200

    Defined app.ts

---
+++ app.ts
@@ -0,0 +1,19 @@
+import config from 'config';
+import express from 'express';
+import connect from "./db/connect";
+
+const port = config.get('port') as number;
+const host = config.get('host') as string;
+
+const app = express();
+app.use(deserializeUser);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(port, host, () => {
+    log.info(`Server listening at http://${host}:${port}`);
+    connect();
+    sampleRoute(app);
+    userRoute(app);
+})
