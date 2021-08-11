3e37b34a49ea4f3fbf56c6e8d8023450fce6701b
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-11 19:06:01 +0200

    sample model defined

---
+++ src/schema/sample.schema.ts
@@ -0,0 +1,37 @@
+import { object, string, date, boolean } from "yup";
+
+const payload = {
+  body: object({
+    task: string().required("Task must be defined"),
+    deadline: date()
+      .min(new Date())
+      .max(
+        new Date(
+          new Date().getFullYear() + 1,
+          new Date().getMonth(),
+          new Date().getDate()
+        )
+      )
+      .required("Deadline must be defined"),
+    isCompleted: boolean(),
+  }),
+};
+
+const params = {
+  params: object({
+    itemId: string().required("itemId is required"),
+  }),
+};
+
+export const createSampleSchema = object({
+  ...payload,
+});
+
+export const updateSampleSchema = object({
+  ...params,
+  ...payload,
+});
+
+export const deleteSampleSchema = object({
+  ...params,
+});
