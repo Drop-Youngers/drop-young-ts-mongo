3f9356b0438d326278289e7d6cc9b9f4fbe4c17f
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-11 19:02:10 +0200

    defined the sample model

---
+++ src/model/sample.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+import { UserDocument } from "./user.model";
+
+export interface SampleDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  task: string;
+  deadline: Date;
+  isCompleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SampleSchema = new mongoose.Schema(
+  {
+    itemId: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => nanoid(10),
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    task: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Sample = mongoose.model<SampleDocument>("Sample", SampleSchema);
+
+export default Sample;
