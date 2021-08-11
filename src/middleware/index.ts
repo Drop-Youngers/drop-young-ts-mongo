dfb042f18a0fd0624a0a497aaf8e6c5e3e57ace6
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-11 22:11:14 +0200

    exporting middlewares

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,4 @@
+export { default as errorHandler } from "./errorHandler";
 export { default as deserializeUser } from "./deserializeUser";
+export { default as requiresUser } from "./requiresUser";
+export { default as validateRequest } from "./validateRequest";
