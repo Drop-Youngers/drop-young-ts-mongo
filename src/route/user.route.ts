b4fd14b8286bda02cf77c0d754dbde1aae3d7afd
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-11 22:16:31 +0200

    all routes in users

--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,2 +1,28 @@
 import { Express, Request, Response } from "express";
-export default function (app: Express) {}
+import { createUserHandler } from "../controller/user.controller";
+import { validateRequest, requiresUser } from "../middleware";
+import {
+  createUserSchema,
+  createUserSessionSchema,
+} from "../schema/user.schema";
+import {
+  createUserSessionHandler,
+  invalidateUserSessionHandler,
+  getUserSessionsHandler,
+} from "../controller/session.controller";
+
+export default function (app: Express) {
+  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/login",
+    validateRequest(createUserSessionSchema),
+    createUserSessionHandler
+  );
+
+  app.get("/api/currentUser", requiresUser, getUserSessionsHandler);
+
+  app.delete("/api/logout", requiresUser, invalidateUserSessionHandler);
+}
